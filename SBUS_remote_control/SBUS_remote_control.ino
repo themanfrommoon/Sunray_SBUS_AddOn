4368f48e66953f49d53d9bd6ce5b3aec63649553
themanfrommoon <christianwulff@gmx.de>
2023-08-05 17:30:42 +0200

    Update SBUS_remote_control.ino

--- SBUS_remote_control/SBUS_remote_control.ino
+++ SBUS_remote_control/SBUS_remote_control.ino
@@ -14,30 +14,46 @@ bfs::SbusRx sbus_rx(&Serial1, 16, 17, true);  // declaration of rx and tx pins b
 bfs::SbusData data;
 
 short throttle_min = 172;     // determined value on my setup for throttle minimum position
-short throttle_mid = 805;     // determined value on my setup for throttle middle position
+short throttle_mid = 992;     // determined value on my setup for throttle middle position
 short throttle_max = 1811;    // determined value on my setup for throttle maximum position
 short steering_left = 172;    // determined value on my setup for steering left position
-short steering_mid = 990;     // determined value on my setup for steering middle position
+short steering_mid = 992;     // determined value on my setup for steering middle position
 short steering_right = 1811;  // determined value on my setup for steering right position
-short mow_full = 1811;        // determined value on my setup for mow motor full power position
-short mow_off = 172;          // determined value on my setup for mow motor off position
+short mode_automatic = 1811;  // determined value on my setup for automatic sunray control
+short mode_move = 992;        // determined value on my setup for manual override only move control
+short mode_mow = 172;         // determined value on my setup for manual override move and mow
 byte throttle_deadband = 20;  // deadband for throttle middle-position to avoid slow movement around middle-position, and both end positions
 byte steering_deadband = 20;  // deadband for steering middle-position to avoid slow movement around middle-position, and both end positions
 short throttle_stick;         // input throttle stick
 short steering_stick;         // input steering stick
-short mow_pot;                // input potentiometer mow motor
+short mode_switch;            // input mode switch
 short throttle;               // throttle value
 short steering;               // steering value
 short expo_throttle = 0;      // expects a value between -100 and +100. recommend value= 0, 0=linear proportional curve, -100=progressive curve in the middle, +100=degressive curve in the middle
-short expo_steering = 0;     // expects a value between -100 and +100. recommend value=30, 0=linear proportional curve, -100=progressive curve in the middle, +100=degressive curve in the middle
+short expo_steering = 30;     // expects a value between -100 and +100. recommend value=30, 0=linear proportional curve, -100=progressive curve in the middle, +100=degressive curve in the middle
 short throttlecurve[2048];    // throttlecurve points array to save calculation time in loop
 short steeringcurve[2048];    // steeringcurve points array to save calculation time in loop
 short throttlewithexpo;       // throttle value mixed with expo
 short steeringwithexpo;       // steering value mixed with expo
-short motor_mow;              // output for right motor
+byte control_mode;            // output for control mode
 short motor_left;             // output for left motor
 short motor_right;            // output for right motor
 
+// send short integer over I2C
+void sendShort(short value) {
+  Wire.write((byte)(value >> 8)); // send highest 8 bits
+  Wire.write((byte)(value & 0xFF));  // send lowest 8 bits
+}
+
+// I2C slave callback
+void I2C_TxHandler(void)
+{
+  //sendShort(throttle);
+  //sendShort(steering);
+  sendShort(motor_left);
+  sendShort(motor_right);
+  sendShort(control_mode);
+}
 
 short expo_curve(short x, short expo) {                // expects input values from -1023 to +1023 and outputs values from -1023 to +1023. The 'only' change is the proportionality factor between input and output
   short y;                                             // EXPO curve development see here: https://forum.arduino.cc/t/expo-einstellung-rc-mathematik/1150995
@@ -54,7 +70,6 @@ short expo_curve(short x, short expo) {                // expects input values f
   return y;
 }
 
-
 void setup() {
   Serial.begin(115200);
   while (!Serial) {}
@@ -63,28 +78,9 @@ void setup() {
     throttlecurve[x + 1023] = expo_curve(x, expo_throttle);
     steeringcurve[x + 1023] = expo_curve(x, expo_steering);
   }
-  // ----------- ADDED ------------------------------
   Wire.begin(0x55); // Initialize I2C (Slave Mode: address=0x55 )
   Wire.onRequest(I2C_TxHandler);
 }
-
-// send short integer over I2C
-void sendShort(short value){
-  Wire.write((byte)(value >> 8)); // send highest 8 bits
-  Wire.write((byte)(value & 0xFF));  // send lowest 8 bits
-}
-
-// I2C slave callback
-void I2C_TxHandler(void)
-{
-  sendShort(throttle);
-  sendShort(steering);
-  sendShort(motor_left);
-  sendShort(motor_right);
-  sendShort(motor_mow);
-}
-
-// --------------ADDED END----------------------------
 
 void loop () {
   if (sbus_rx.Read()) {
@@ -113,9 +109,18 @@ void loop () {
       }
     };
 
-    // --------------------mowmotor potentiometer--------------------
-    mow_pot = data.ch[4];
-    motor_mow = (map(mow_pot, mow_off, mow_full, 0, 1023));
+    // --------------------mode switch--------------------
+    mode_switch = data.ch[4];
+    if (mode_switch < (mode_move - mode_mow) / 2 + mode_mow) {
+      control_mode = 2;          // control mode "manual override move and mow"
+    }
+    else {
+      if (mode_switch < (mode_automatic - mode_move) / 2 + mode_move && mode_switch > (mode_move - mode_mow) / 2 + mode_mow) {
+        control_mode = 1;        // control mode "manual override only move"
+      } else {
+        control_mode = 0;      // control mode "automatic sunray control"
+      }
+    }
 
     // --------------------failsafe--------------------
     if (data.failsafe > 0) {
@@ -124,7 +129,8 @@ void loop () {
       steering = 0;
       motor_left = 0;
       motor_right = 0;
-      motor_mow = 0;
+      control_mode = 0;
+
     }
     else {
       Serial.print("running");    // if no failsafe is recognized by the FrSky receiver
@@ -135,46 +141,54 @@ void loop () {
     steeringwithexpo = steeringcurve[steering + 1023];
 
     // --------------------cross-mixer left motor--------------------
-    if (throttlewithexpo + steeringwithexpo > 0) {                 // if the sum of throttlewithexpo and steeringwithexpo is forward
-      if (throttlewithexpo + steeringwithexpo < 1023) {            // if the sum of throttlewithexpo and steeringwithexpo is less the full forward
-        motor_left = throttlewithexpo + steeringwithexpo;          // then motor_left = throttlewithexpo + steeringwithexpo
-      } else {
-        motor_left = 1023;                                         // else motor_left =  full forward
+    if (control_mode > 0) {
+      if (throttlewithexpo + steeringwithexpo > 0) {                 // if the sum of throttlewithexpo and steeringwithexpo is forward
+        if (throttlewithexpo + steeringwithexpo < 1023) {            // if the sum of throttlewithexpo and steeringwithexpo is less the full forward
+          motor_left = throttlewithexpo + steeringwithexpo;          // then motor_left = throttlewithexpo + steeringwithexpo
+        } else {
+          motor_left = 1023;                                         // else motor_left =  full forward
+        }
+      } else {                                                       // else the sum of throttlewithexpo and steeringwithexpo is backward
+        if (throttlewithexpo + steeringwithexpo > -1023) {           // if the sum throttlewithexpo and steeringwithexpo is less then full backwards
+          motor_left = throttlewithexpo + steeringwithexpo;          // then motor_left = throttlewithexpo + steeringwithexpo
+        } else {
+          motor_left = -1023;                                        // else motor_left = full backwards
+        }
       }
-    } else {                                                       // else the sum of throttlewithexpo and steeringwithexpo is backward
-      if (throttlewithexpo + steeringwithexpo > -1023) {           // if the sum throttlewithexpo and steeringwithexpo is less then full backwards
-        motor_left = throttlewithexpo + steeringwithexpo;          // then motor_left = throttlewithexpo + steeringwithexpo
-      } else {
-        motor_left = -1023;                                        // else motor_left = full backwards
-      }
+    } else {
+      motor_left = 0;
     }
 
     // --------------------cross-mixer right motor--------------------
-    if (throttlewithexpo - steeringwithexpo > 0) {                 // if the difference between throttlewithexpo and steeringwithexpo is forward
-      if (throttlewithexpo - steeringwithexpo < 1023) {            // if the difference between throttlewithexpo and steeringwithexpo is less the full forward
-        motor_right = throttlewithexpo - steeringwithexpo;         // then motor_right = throttlewithexpo - steeringwithexpo
-      } else {
-        motor_right = 1023;                                        // else motor_right =  full forward
+    if (control_mode > 0) {
+      if (throttlewithexpo - steeringwithexpo > 0) {                 // if the difference between throttlewithexpo and steeringwithexpo is forward
+        if (throttlewithexpo - steeringwithexpo < 1023) {            // if the difference between throttlewithexpo and steeringwithexpo is less the full forward
+          motor_right = throttlewithexpo - steeringwithexpo;         // then motor_right = throttlewithexpo - steeringwithexpo
+        } else {
+          motor_right = 1023;                                        // else motor_right =  full forward
+        }
+      } else {                                                       // else the difference between throttlewithexpo and steeringwithexpo is backward
+        if (throttlewithexpo - steeringwithexpo > -1023) {           // if the difference between throttlewithexpo and steeringwithexpo is less then full backwards
+          motor_right = throttlewithexpo - steeringwithexpo;         // then motor_right = throttlewithexpo - steeringwithexpo
+        } else {
+          motor_right = -1023;                                       // else motor_right = full backwards
+        }
       }
-    } else {                                                       // else the difference between throttlewithexpo and steeringwithexpo is backward
-      if (throttlewithexpo - steeringwithexpo > -1023) {           // if the difference between throttlewithexpo and steeringwithexpo is less then full backwards
-        motor_right = throttlewithexpo - steeringwithexpo;         // then motor_right = throttlewithexpo - steeringwithexpo
-      } else {
-        motor_right = -1023;                                       // else motor_right = full backwards
-      }
+    } else {
+      motor_right = 0;
     }
 
-    // --------------------send values on I²C--------------------
+    // --------------------serial print values--------------------
     Serial.print("\t");
     Serial.print("\t");
-    Serial.print(throttle);
-    Serial.print("\t");
-    Serial.print(steering);
-    Serial.print("\t");
+    //Serial.print(throttle);
+    //Serial.print("\t");
+    //Serial.print(steering);
+    //Serial.print("\t");
     Serial.print(motor_left);
     Serial.print("\t");
     Serial.print(motor_right);
     Serial.print("\t");
-    Serial.println(motor_mow);
+    Serial.println(control_mode);
   }
 }
